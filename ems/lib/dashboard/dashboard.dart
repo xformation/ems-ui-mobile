a6d37773add16ce70fbe0ac9730a02d769dc7108
Dipti Patel <diptippatel96@gmail.com>
2020-01-30 17:29:22 +0530

    merge chart and dashboard page

--- ems/lib/dashboard/dashboard.dart
+++ ems/lib/dashboard/dashboard.dart
@@ -1,110 +1,156 @@
 import 'package:flutter/material.dart';
 import 'package:ems/theme_data.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
-class Dashboard {
-  Dashboard();
-  Widget createdashboard(BuildContext context) {
-    return new Container(
-        color: Color.fromARGB(1, 250, 250, 248),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(
-                  left: 30.0, right: 27.0, top: 20.0, bottom: 20.0),
-              child: Text("Welcome Richard Grand!",
-                  style: TextStyle(
-                      color: LocalTheme.dashboard["heading"]["color"],
-                      fontWeight: LocalTheme.dashboard["heading"]
-                          ["font_weight"],
-                      fontFamily: LocalTheme.dashboard["heading"]
-                          ["font_family"],
-                      fontSize: 28)),
-            ),
-            Container(
-              height: 220.0,
-              margin: EdgeInsets.only(
-                  left: 20.0, right: 20.0, top: 0.0, bottom: 20.0),
-              // margin: EdgeInsets.all(15.0),
-              color: Colors.white,
-              child: ListView(children: <Widget>[
-                ListTile(
-                  dense: true,
-                    title: Text("Select Student",
-                        style: TextStyle(
-                            color: LocalTheme.dashboard["sub_heading"]["color"],
-                            fontWeight: LocalTheme.dashboard["sub_heading"]
-                                ["font_weight"],
-                            fontFamily: LocalTheme.dashboard["sub_heading"]
-                                ["font_family"],
-                            fontSize: 18))),
-                ListTile(
-                  leading: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      minWidth: 44,
-                      minHeight: 44,
-                      maxWidth: 64,
-                      maxHeight: 64,
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => new _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  List<charts.Series<OrdinalSales, String>> seriesList;
+  final bool animate = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _createSampleData();
+  }
+
+  _createSampleData() {
+    final data = [
+      new OrdinalSales('Jan', 5),
+      new OrdinalSales('Feb', 25),
+      new OrdinalSales('Mar', 100),
+      new OrdinalSales('Apr', 40),
+      new OrdinalSales('May', 50),
+      new OrdinalSales('Jun', 60),
+      new OrdinalSales('Jul', 25),
+      new OrdinalSales('Aug', 35),
+      new OrdinalSales('Sep', 45),
+      new OrdinalSales('Oct', 85),
+      new OrdinalSales('Now', 95),
+      new OrdinalSales('Dec', 10),
+    ];
+
+    seriesList = [
+      charts.Series<OrdinalSales, String>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.yellow.shadeDefault,
+        domainFn: (OrdinalSales sales, _) => sales.month,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromARGB(1, 250, 250, 248),
+      child: SingleChildScrollView(
+          child: Column(children: <Widget>[
+        Container(
+          margin:
+              EdgeInsets.only(left: 30.0, right: 27.0, top: 20.0, bottom: 20.0),
+          child: Text("Welcome Richard Grand!",
+              style: TextStyle(
+                  color: LocalTheme.dashboard["heading"]["color"],
+                  fontWeight: LocalTheme.dashboard["heading"]["font_weight"],
+                  fontFamily: LocalTheme.dashboard["heading"]["font_family"],
+                  fontSize: 28)),
+        ),
+        Container(
+            height: 220.0,
+            margin: EdgeInsets.only(
+                left: 20.0, right: 20.0, top: 0.0, bottom: 20.0),
+            // margin: EdgeInsets.all(15.0),
+            color: Colors.white,
+            child: ListView(children: <Widget>[
+                    ListTile(
+                        dense: true,
+                        title: Text("Select Student",
+                            style: TextStyle(
+                                color: LocalTheme.dashboard["sub_heading"]
+                                    ["color"],
+                                fontWeight: LocalTheme.dashboard["sub_heading"]
+                                    ["font_weight"],
+                                fontFamily: LocalTheme.dashboard["sub_heading"]
+                                    ["font_family"],
+                                fontSize: 18))),
+                    ListTile(
+                      leading: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth: 44,
+                          minHeight: 44,
+                          maxWidth: 64,
+                          maxHeight: 64,
+                        ),
+                        child: Image.asset('assets/images/Image.png',
+                            fit: BoxFit.cover),
+                      ),
+                      title: Text("Sara Adamas",
+                          style: TextStyle(
+                              color: LocalTheme.dashboard["student_name"]
+                                  ["color"],
+                              fontWeight: LocalTheme.dashboard["student_name"]
+                                  ["font_weight"],
+                              fontFamily: LocalTheme.dashboard["student_name"]
+                                  ["font_family"],
+                              fontSize: 16)),
+                      subtitle: Text("8th Grade, Telangana State Boardd",
+                          style: TextStyle(
+                              color: LocalTheme.dashboard["student_description"]
+                                  ["color"],
+                              fontSize: 12,
+                              fontFamily:
+                                  LocalTheme.dashboard["student_description"]
+                                      ["font_family"])),
                     ),
-                    child: Image.asset('assets/images/Image.png',
-                        fit: BoxFit.cover),
-                  ),
-                  title: Text("Sara Adamas",
-                      style: TextStyle(
-                          color: LocalTheme.dashboard["student_name"]["color"],
-                          fontWeight: LocalTheme.dashboard["student_name"]
-                              ["font_weight"],
-                          fontFamily: LocalTheme.dashboard["student_name"]
-                              ["font_family"],
-                          fontSize: 16)),
-                  subtitle: Text("8th Grade, Telangana State Boardd",
-                      style: TextStyle(
-                          color: LocalTheme.dashboard["student_description"]
-                              ["color"],
-                          fontSize: 12,
-                          fontFamily:
-                              LocalTheme.dashboard["student_description"]
-                                  ["font_family"])),
-                ),
-                Divider(),
-                ListTile(
-                  leading: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      minWidth: 44,
-                      minHeight: 44,
-                      maxWidth: 64,
-                      maxHeight: 64,
+                    Divider(),
+                    ListTile(
+                      leading: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth: 44,
+                          minHeight: 44,
+                          maxWidth: 64,
+                          maxHeight: 64,
+                        ),
+                        child: Image.asset('assets/images/Image2.png',
+                            fit: BoxFit.cover),
+                      ),
+                      title: Text("Kevin Dean",
+                          style: TextStyle(
+                              color: LocalTheme.dashboard["student_name"]
+                                  ["color"],
+                              fontWeight: LocalTheme.dashboard["student_name"]
+                                  ["font_weight"],
+                              fontFamily: LocalTheme.dashboard["student_name"]
+                                  ["font_family"],
+                              fontSize: 16)),
+                      subtitle: Text("10th Grade, Telangana State Board",
+                          style: TextStyle(
+                              color: LocalTheme.dashboard["student_description"]
+                                  ["color"],
+                              fontSize: 12,
+                              fontFamily:
+                                  LocalTheme.dashboard["student_description"]
+                                      ["font_family"])),
                     ),
-                    child: Image.asset('assets/images/Image2.png',
-                        fit: BoxFit.cover),
-                  ),
-                  title: Text("Kevin Dean",
-                      style: TextStyle(
-                          color: LocalTheme.dashboard["student_name"]["color"],
-                          fontWeight: LocalTheme.dashboard["student_name"]
-                              ["font_weight"],
-                          fontFamily: LocalTheme.dashboard["student_name"]
-                              ["font_family"],
-                          fontSize: 16)),
-                  subtitle: Text("10th Grade, Telangana State Board",
-                      style: TextStyle(
-                          color: LocalTheme.dashboard["student_description"]
-                              ["color"],
-                          fontSize: 12,
-                          fontFamily:
-                              LocalTheme.dashboard["student_description"]
-                                  ["font_family"])),
-                ),
-              ]),
-            ),
-            Container(
-              height: 220.0,
-              margin: EdgeInsets.only(
-                  left: 20.0, right: 20.0, top: 0.0, bottom: 20.0),
-              color: Colors.white,
-              child: ListView(children: <Widget>[
+                  ])),
+                // ])),
+        Container(
+            height: 220.0,
+            margin: EdgeInsets.only(
+                left: 20.0, right: 20.0, top: 0.0, bottom: 20.0),
+            color: Colors.white,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                    Widget>[
+              Expanded(
+                  child: ListView(children: <Widget>[
                 ListTile(
-                   dense: true,
+                    dense: true,
                     title: Text("News and Notifications",
                         style: TextStyle(
                             color: LocalTheme.dashboard["sub_heading"]["color"],
@@ -164,16 +210,33 @@ class Dashboard {
                               LocalTheme.dashboard["student_description"]
                                   ["font_family"])),
                 ),
-              ]),
-            ),
-          ],
-        )
-        // child: Container(
-
-        // ),
-        // child:Container(
-
-        // )
-        );
+              ])),
+            ])),
+        Container(
+            height: 220.0,
+            margin: EdgeInsets.only(
+                left: 20.0, right: 20.0, top: 0.0, bottom: 20.0),
+            color: Colors.white,
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                      child: charts.BarChart(
+                    seriesList,
+                    animate: animate,
+                    primaryMeasureAxis: charts.NumericAxisSpec(
+                        renderSpec: charts.NoneRenderSpec()),
+                  )),
+                ]))
+      ])),
+    );
   }
+}
+
+class OrdinalSales {
+  final String month;
+  final int sales;
+
+  OrdinalSales(this.month, this.sales);
 }
--- ems/lib/home/home.dart
+++ ems/lib/home/home.dart
@@ -1,43 +1,11 @@
-import 'package:ems/containers/menu.dart';
-import 'package:ems/containers/slide_scaffold.dart';
-import 'package:ems/controller/menu_controller.dart';
+import 'package:ems/containers/parent_container.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:ems/dashboard/dashboard.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => new _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  MenuController _menuController;
+class HomePage extends StatelessWidget {
 
   @override
-  void initState() {
-    super.initState();
-    _menuController = new MenuController(vsync: this);
-    _menuController.addListener(() {
-      return setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    _menuController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final dashboard = Dashboard();
-    return ChangeNotifierProvider(
-        builder: (context) => _menuController,
-        child: SlideScaffold(
-            menuScreen: Menu(),
-            contentBuilder: (cc) => Container(
-                  //  padding: EdgeInsets.all(16.0),
-                  child: dashboard.createdashboard(context),
-                )));
+  Widget build(BuildContext context){
+    return ParentContainer(Dashboard());
   }
 }
