f696e8f0e3234a2fab026e81287ac4e359621b19
Dipti Patel <diptippatel96@gmail.com>
2020-02-03 12:03:04 +0530

    add dashboard 3 page design

--- ems/lib/main.dart
+++ ems/lib/main.dart
@@ -1,8 +1,10 @@
+
 import 'package:ems/business/store.dart';
 import 'package:ems/dashboard/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:ems/home/home.dart';
 import 'package:ems/attendance/mark_attendance.dart';
+import 'package:ems/dashboarddetail/dashboarddetail.dart';
 import 'package:ems/containers/parent_container.dart';
 
 void main() {
@@ -10,6 +12,7 @@ void main() {
   runApp(MaterialApp(initialRoute: "/", routes: {
     '/': (context) => ParentContainer(Home()),
     '/dashboard': (context) => ParentContainer(Dashboard()),
-    '/attendance': (context) => ParentContainer(MarkAttendancePage(store))
+    '/attendance': (context) => ParentContainer(MarkAttendancePage(store)),
+    '/dashboardDetail': (context) => ParentContainer(DashboardDetail()),
   }));
 }
